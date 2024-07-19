b2c983fc1434f5507bf65fa9e68de1677e719309
Naman Sharma <120219878+NamanSharma1201@users.noreply.github.com>
2024-07-19 21:19:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,34 @@
-import dotenv from "dotenv";
 import express from "express";
+import path from "path";
+import dotenv from "dotenv";
+import codeRouter from "./routes/code.js";
+import makeFolders from "./services/makeFolders.js";
 import cors from "cors";
-import cookieParser from "cookie-parser";
-import connectDB from "./config/connectDB.js";
-import userRouter from "./routes/user.js";
-import problemRouter from "./routes/problem.js";
-import commentRouter from "./routes/comments.js";
-import blogRouter from "./routes/blogs.js";
 
 dotenv.config();
 
-const port = process.env.PORT || 8000;
 const app = express();
+const port = process.env.PORT || 5000;
 
-connectDB();
-
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cookieParser());
 
-app.use("/api/user", userRouter);
-app.use("/api/problem", problemRouter);
-app.use("/blog", blogRouter);
-app.use("/comment", commentRouter);
+makeFolders();
+
+// Main router
+app.use("/", codeRouter);
+
+// Error handling middleware
 
 app.listen(port, () => {
-  console.log(`API server running on port ${port} ....`);
+  console.log(`Online-Compiler running on port ${port}`);
 });
